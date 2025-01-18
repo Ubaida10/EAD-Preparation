66e9224a093a51686920ade44b9d5597032fd69c
Ubaida10 <123815303+Ubaida10@users.noreply.github.com>
2025-01-18 14:48:27 +0500

    Removed

--- API/GraphQL/GraphQL_API/Models/Author.cs
+++
@@ -1,8 +0,0 @@
-namespace GraphQL_API.Models;
-
-public class Author
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<Post> Posts { get; set; } = new(); // Navigation property for related Posts
-}
--- API/GraphQL/GraphQL_API/Models/Comment.cs
+++
@@ -1,9 +0,0 @@
-namespace GraphQL_API.Models;
-
-public class Comment
-{
-    public int Id { get; set; }
-    public string Text { get; set; }
-    public int PostId { get; set; } // Foreign key for Post
-    public Post Post { get; set; } // Navigation property
-}
